a44bf4ac527144d94870b06d483686b8d03cf4fb
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-10-23 11:04:01 +0000

    Initial commit

---
+++ run.py
@@ -0,0 +1,36 @@
+import os
+
+# First import flask class
+from flask import Flask, render_template
+
+# Creat an instance of this and store it in a variable called app. 
+# The first argument is the name of the application module
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
+
+
